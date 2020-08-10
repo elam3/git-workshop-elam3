0d8a27553cb76a83af48523f66869e66a1b54050
elam3 <elam3@mail.ccsf.edu>
2020-08-10 10:27:57 -0700

    edit shell script

--- two.sh
+++ two.sh
@@ -1,19 +1,44 @@
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum orci ut mi porttitor, et faucibus quam posuere. Vivamus dolor diam, ultricies elementum hendrerit feugiat, tristique non est. Donec et congue odio. Nam in dolor sed diam posuere dictum. Praesent quis facilisis ipsum, vel suscipit enim. Integer ante erat, finibus et posuere in, vehicula in lacus. Phasellus non mi eu velit fermentum tempor et commodo massa. Suspendisse ut turpis lacinia, blandit justo ac, tincidunt augue. Maecenas ut lacus dapibus, vulputate ante id, pellentesque lacus. Nulla mattis urna eros, tristique bibendum lacus suscipit nec.
+#!/bin/bash
+# Run this command to see your slurm associations:
+#   sacctmgr show assoc -p | grep wsyuweb
+#
+# and then configure the next three SBATCH options accordingly.
+# (1) Which partition do you want to use? 
+#   If you can, choose lr3. But if none of the nodes are available,
+#   then feel free to try lr4, or lr5, or l6.
+#   Use this command to check for idle (available) nodes:
+#      sinfo -p lr3,lr4,lr5,lr6 | grep idle
+#
+#SBATCH --partition=lr6
+# (2) Which account do you want to use?
+#   You should see two accounts associated with your user name:
+#      ac_fwi, and pc_exascalecomputing.
+#   Run this command to see if you have any free hours (SUs for service units)
+#   remaining on an account:
+#      check_usage.sh -h           # See the help for more details
+#      check_usage.sh -a ac_fwi
+#      check_usage.sh -a pc_exascalecomputing
+#      check_usage.sh -a "ac_fwi,pc_exascalecomputing"
+#
+#SBATCH --account=ac_fwi
+# (3) Which quality of service to use?
+#   Usually lr_normal is fine.
+#
+#SBATCH --qos=lr_normal
+# (4) Feel free to change the name of this job. It's only for your own reference.
+#
+#SBATCH --job-name="this ia a test"
+# (5) How long does this job expect to run for? 
+#
+# Acceptable  time  formats  include "minutes", "minutes:seconds",
+# "hours:minutes:seconds", "days-hours", "days-hours:minutes" and 
+# "days-hours:minutes:seconds".
+#
+# e.g. --time="2-0:1:2" # Request for two days, zero hours, 1 minute, 2 seconds
+#
+#SBATCH --time="2-0:1:2"
+# Get email notifications when your jobs start/end.
+#SBATCH --mail-user=suiwenw@unr.edu
+#SBATCH --mail-type=all
 
-Ut nec blandit dolor. Mauris varius molestie dignissim. Ut ac nunc in elit pretium sollicitudin. Suspendisse scelerisque sollicitudin tristique. In ante dolor, tristique mattis venenatis non, maximus a arcu. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam viverra, magna sed pharetra pulvinar, turpis sem pellentesque neque, a posuere neque nulla et nibh. Nam volutpat nisi a dapibus posuere. Morbi luctus tellus dui, at maximus dolor fringilla volutpat. Integer semper lorem eget nibh egestas mollis.
-
-Sed iaculis dui nulla, in fringilla dolor molestie ac. Proin aliquam neque ut justo pellentesque, a lobortis ex bibendum. Aenean nec semper massa. Pellentesque sit amet mi mi. Ut gravida odio vitae mi posuere sodales. Cras nec condimentum lacus. Fusce imperdiet, tortor id lobortis rhoncus, ex libero tristique risus, vitae porttitor nunc velit posuere neque. Nunc sodales mauris sit amet blandit luctus. Nunc consequat, ligula quis varius tempor, tellus arcu luctus enim, et pretium magna orci eu tellus. Duis id ligula nec est consectetur tempor.
-
-Aenean accumsan, magna sit amet aliquam condimentum, libero eros ultricies enim, eget bibendum mauris sapien sit amet lacus. Nulla placerat interdum eros, laoreet euismod dui hendrerit in. Donec tincidunt purus nec ipsum mattis, a vulputate neque rutrum. Praesent ullamcorper, nulla ac varius fringilla, justo tellus sollicitudin tortor, sit amet commodo elit mi a quam. Suspendisse pellentesque sollicitudin lectus sed mattis. Pellentesque eros arcu, fringilla vel sollicitudin sit amet, pulvinar at nisi. Maecenas ac tincidunt purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam venenatis ac enim id molestie. Suspendisse eu est eleifend nisl fringilla pellentesque. Integer gravida, dui non elementum euismod, nunc libero pharetra dui, vitae vehicula nibh diam vel neque.
-
-Quisque dapibus felis quis nulla mollis porta. Vivamus iaculis nisl enim, a tempor lacus varius vitae. Donec ac lacus maximus, interdum ligula eu, euismod enim. Sed nibh nibh, condimentum id justo et, ullamcorper maximus dolor. Fusce scelerisque hendrerit aliquam. Nam efficitur consectetur neque. Pellentesque sodales ligula sit amet hendrerit gravida. Nullam faucibus odio eget sapien iaculis viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla sit amet rhoncus neque. Aliquam mi sem, laoreet a ante eget, euismod condimentum leo. Ut euismod scelerisque nisi eu mollis. Aliquam erat volutpat. Nunc eu malesuada urna, nec dapibus mi. Nullam bibendum quam vel risus tempor, non ultricies metus semper. Sed aliquam leo sit amet tortor tempor pulvinar.
-
-Sed tincidunt semper eros vitae ullamcorper. Vestibulum sit amet lacus congue, fermentum dolor at, sodales ligula. Nam ultricies sem et lacus pellentesque tincidunt. Proin vel ex ut sem hendrerit ornare. Praesent vel enim a ex porttitor porta accumsan a arcu. Pellentesque maximus lorem eu accumsan aliquet. Integer facilisis tincidunt tortor, vitae gravida sapien posuere ultricies. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
-
-Duis commodo euismod lectus, in aliquet nisl. Pellentesque vulputate sapien bibendum, dictum est a, efficitur neque. Sed vulputate neque diam, id laoreet metus hendrerit et. Etiam pellentesque est et bibendum ultricies. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec commodo volutpat lorem, ut ullamcorper erat rhoncus a. Phasellus malesuada eros id condimentum scelerisque. Maecenas posuere elementum est eu porttitor. Nullam rhoncus vehicula tellus, in volutpat orci laoreet nec. Aenean velit magna, semper vel libero eu, congue maximus felis. Pellentesque diam augue, efficitur sed velit at, sagittis blandit mi. Integer luctus maximus odio, eu varius dui dictum rutrum. Curabitur eu convallis velit, vitae tempor elit. Vestibulum hendrerit efficitur arcu, in eleifend tortor interdum id.
-
-Fusce pellentesque malesuada tempor. Nunc mattis lectus non mi elementum fringilla. Mauris malesuada sapien nec leo tincidunt, ut pulvinar odio congue. Proin in neque quis felis sagittis fringilla. Sed sit amet commodo diam. Vestibulum tincidunt justo vel erat rutrum, et facilisis urna imperdiet. Proin eget massa lacus. Pellentesque sodales iaculis laoreet. Pellentesque dignissim nulla nec euismod fermentum. Pellentesque feugiat sapien nec iaculis ullamcorper. Morbi ultrices diam sed lectus condimentum egestas. Maecenas scelerisque non ipsum eu mollis. In mi enim, hendrerit vitae vestibulum et, iaculis eu risus. Nullam purus augue, interdum porta ante id, ultricies tempor elit. Phasellus suscipit eros hendrerit dignissim aliquam. In hac habitasse platea dictumst.
-
-Quisque iaculis sem tellus. Quisque lorem nibh, sollicitudin eu lobortis at, convallis vel massa. Suspendisse nunc nisl, dapibus eget felis ut, mollis elementum tellus. Etiam pharetra vel nunc at dictum. In sodales ligula in sem elementum, et sodales neque ultrices. Nulla ac eros consequat, luctus ipsum eget, iaculis sem. Phasellus lacus est, tempor vitae mi id, consectetur maximus est.
-
-In vitae neque malesuada, efficitur nibh id, cursus nibh. Donec lobortis, eros volutpat congue dapibus, mauris arcu vulputate eros, id volutpat urna nunc in dui. Cras in commodo metus, vitae blandit purus. Morbi iaculis at dui quis auctor. Donec id magna sem. In ultricies condimentum eros a semper. Etiam a nunc sed lacus pharetra tincidunt at nec nunc. Fusce ultricies nulla at urna tempus, nec venenatis dolor volutpat.
+time singularity exec /global/scratch/elam3/sif/Real-ESSI-v20.07.sif mpirun -np 20 pessi -f 3story.fei 
